0dc2c9db7839d0e885163944c3b1742617bb78cd
Wilson L. Gasmido Jr. <165973006+Gasmido@users.noreply.github.com>
2026-01-11 19:09:56 +0800

    Add files via upload

---
+++ topbarAdmin.php
@@ -0,0 +1,44 @@
+<body>
+  
+	<!--
+    <div class="float-parent-element">
+  <div class="float-child-element">
+    <div class="red">
+      <img class="logoo3" src="image/logoo.png">
+      Barangay Ayos Lomboy
+    </div>
+  </div>
+  <div class="float-child-element">
+    <div class="yellow">
+      <nav>
+  <ul>
+    <li><a href="#"><i class='bx bx-cog'></i></a>
+      <ul>
+        <li><a class="ac" href="logout">Log-out</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+</div>
+</div>
+</div>
+-->
+<div class="container-fluid nav-bar bg-transparent sticky-top" style="margin-top:0;">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4" style="margin-bottom: 0;">
+                <a href="Home-page" class="navbar-brand d-flex align-items-center text-center">
+                    <div class="icon p-2 me-2">
+                        <img class="img-fluid" src="img/logoo.png" alt="Icon" style="width: 30px; height: 30px;">
+                    </div>
+                    <h1 class="m-0 text-primary">Ayos Lomboy</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto">
+                        <a href="logout" class="nav-item nav-link active">Logout</a>                     
+                    </div>
+                </div>
+            </nav>
+        </div>
+
